4210ab82e0711040250022e284ca9ac7d1f44d12
Alexander Marinskiy <marinskiy@gmail.com>
2022-03-13 22:47:35 +0300

    tmp

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p .artifacts
-cmake -S . -B .artifacts -DBoost_LIBRARY_DIRS="/home/runner/work/csc-system-design/csc-system-design/boost/boost/lib"
+cmake -S . -B .artifacts
 make -C .artifacts
 mv .artifacts/src/shell ./
 mv .artifacts/test/shell_tests ./
