91b183467571161494b7d8c73a52f277fc15bb3b
Alexander Marinskiy <marinskiy@gmail.com>
2022-03-14 09:37:42 +0300

    revert boost

--- src/commands/external_command.cpp
+++ src/commands/external_command.cpp
@@ -1,37 +1,35 @@
 #include "external_command.h"
 #include <iostream>
-#include <boost/process/io.hpp>
-#include <boost/process/system.hpp>
+#include <fstream>
 
-shell::CommandResult shell::ExternalCommand::executeInternalLogic(const VariablesStorage &variables) {
+shell::CommandResult shell::ExternalCommand::executeInternalLogic(const VariablesStorage& variables) {
   std::string commandString;
-  for (const auto&[name, value]: variables.GetVariables()) {
-    commandString += name + "=" + value + " ";
+  for (const auto& [name, value]: variables.GetVariables()) {
+      commandString += name + "=" + value + " ";
   }
-
-  std::stringstream out_stream, err_stream;
+  std::ostringstream out, err;
   for (int i = 0; i < arguments_.size(); i++) {
     commandString += arguments_[i] + " ";
   }
+  commandString += "1>out.log 2>err.log";
 
-  boost::process::opstream in;
-  boost::process::ipstream out, err;
-
-  in << in_stream_;
-
-  boost::process::system(commandString.c_str(),
-                         boost::process::std_out > out,
-                         boost::process::std_err > err,
-                         boost::process::std_in < in);
   int exit_code = std::system(commandString.c_str());
 
-  out_stream << out.rdbuf();
-  err_stream << err.rdbuf();
+  std::ifstream f("out.log");
+  out << f.rdbuf();
+  f.close();
 
-  return {out_stream.str(), err_stream.str(), exit_code, false};
+  std::ifstream e("err.log");
+  err << e.rdbuf();
+  f.close();
+
+  std::remove("out.log");
+  std::remove("err.log");
+
+  return {out.str(), err.str(), exit_code, false};
 }
 
-shell::ExternalCommand::ExternalCommand(std::vector<std::string> &&arguments,
-                                        std::string &&in_stream) :
-    shell::CommandBase::CommandBase(std::move(arguments), std::move(in_stream)) {
+shell::ExternalCommand::ExternalCommand(std::vector<std::string>&& arguments,
+                                        std::string&& in_stream) :
+        shell::CommandBase::CommandBase(std::move(arguments), std::move(in_stream)) {
 };
